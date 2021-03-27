b616244387e1f30e961153e5ae78c18e6008a2fe
Sankalp Prithyani <sankalpprithyani@gmail.com>
2021-03-27 17:42:26 +0530

    style

--- test.php
+++ test.php
@@ -1,10 +1,9 @@
 <?php
-    require("php/connection.php");
-    $result = $sqlConnection->query("SELECT count(email) AS email, count(contact) AS contact FROM donatorcred WHERE email='sankyani@gmail.com';");
-    $resultObj = $result->fetch_object();
-    if($resultObj->email > 0 || $resultObj->contact > 0){
-        echo "0";
-    }else{
-        echo "1";
+    include "php/connection.php";
+    $icons = $sqlConnection->query('SELECT *FROM categories;');
+    <div>
+    while($icon = $icons->fetch_array()){
+        echo '$icon[2]';
     }
+    </div>
 ?>
