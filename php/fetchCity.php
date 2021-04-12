7194453c994b297e66b08fede007c24facad8e44
Sankalp Prithyani <sankalpprithyani@gmail.com>
2021-04-12 10:03:44 +0530

    final

--- php/fetchCity.php
+++ php/fetchCity.php
@@ -3,7 +3,7 @@
     $stateCode = $_POST['stateCode'];
         $cities = $sqlConnection->query("SELECT *FROM city WHERE stateID='$stateCode';");
         while($city = $cities->fetch_array()){
-        $city[1] = str_replace(" ", "_", $city[1]);
-        echo '<option value='."$city[1]".' stateCode='.$stateCode.' onclick="printCity(this)">'.$city[1].'</option>';
+        $userCity = str_replace(" ", "_", $city[1]);
+        echo '<option value='."$userCity".' stateCode='.$stateCode.' onclick="printCity(this)">'.$city[1].'</option>';
     }
 ?>
